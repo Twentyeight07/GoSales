45812d69984d2800bd1f019319715c4d1671dcd2
Twentyeight07 <eduardofreites1999@gmail.com>
2023-04-15 18:04:30 -0400

    UI added

---
+++ GoSales/Controllers/SaleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoSales.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult NewSale()
+        {
+            return View();
+        }
+
+        public IActionResult SaleHistory()
+        {
+            return View();
+        }
+    }
+}
