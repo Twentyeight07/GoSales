8020f49d095e474e14cb9a2ec2990b7c2f8ac524
Twentyeight07 <eduardofreites1999@gmail.com>
2023-04-16 20:05:14 -0400

    Saving

---
+++ Data/Implementation/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.DBContext;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data.Implementation
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly SalesDbContext _dbContext;
+
+        public GenericRepository(SalesDbContext dbContext) 
+        {
+         _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> Get(Expression<Func<TEntity, bool>> filter)
+        {
+            try
+            {
+                TEntity entity = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(filter);
+                return entity;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<TEntity> Create(TEntity entity)
+        {
+            try
+            {
+                _dbContext.Set<TEntity>().Add(entity);
+                await _dbContext.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Edit(TEntity entity)
+        {
+            try
+            {
+                // This code can be changed to _dbContext.Update(entity) but for now I'm going to leave it
+                _dbContext.Set<TEntity>().Update(entity);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<bool> Delete(TEntity entity)
+        {
+            try
+            {
+                _dbContext.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<IQueryable<TEntity>> Consult(Expression<Func<TEntity, bool>> filter = null)
+        {
+            IQueryable<TEntity> queryEntity = filter == null ? _dbContext.Set<TEntity>() : _dbContext.Set<TEntity>().Where(filter);
+            return queryEntity;
+        }
+
+    }
+}
---
+++ Data/Implementation/SaleRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data.DBContext;
+using Data.Interfaces;
+using Entity;
+
+namespace Data.Implementation
+{
+    public class SaleRepository : GenericRepository<Sale>, ISaleRepository
+    {
+        private readonly SalesDbContext _dbContext;
+
+        public SaleRepository(SalesDbContext dbContext):base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Sale> Registry(Sale entity)
+        {
+            Sale generatedSale = new Sale();
+
+            using(var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach(SaleDetail sd in entity.SaleDetails)
+                    {
+                        Product product_found = _dbContext.Products.Where(p=> p.ProductId == sd.ProductId).FirstOrDefault();
+
+                        product_found.Stock = product_found.Stock - sd.Quantity;
+                        _dbContext.Products.Update(product_found);
+                    }
+                    await _dbContext.SaveChangesAsync();
+
+                    CorrelativeNumber correlative = _dbContext.CorrelativeNumbers.Where(n=> n.Management == "sale").First();
+
+                    correlative.LastNumber = correlative.LastNumber + 1;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public Task<List<Sale>> Report(DateTime StartDate, DateTime EndDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
