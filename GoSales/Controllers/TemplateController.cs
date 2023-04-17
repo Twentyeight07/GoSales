8795814bc506fcb59dc2231bb1654f8b525a6d62
Twentyeight07 <eduardofreites1999@gmail.com>
2023-04-17 19:09:27 -0400

    Logic added to the different Interfaces and Implementations.

---
+++ GoSales/Controllers/TemplateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoSales.Controllers
+{
+    public class TemplateController : Controller
+    {
+        public IActionResult SendPassword(string email, string password)
+        {
+            ViewData["Email"] = email;
+            ViewData["Password"] = password;
+            ViewData["Url"] = $"{this.Request.Scheme}://{this.Request.Host}";
+
+            return View();
+        }
+
+        public IActionResult RestorePassword(string password)
+        {
+            ViewData["Password"] = password;
+
+            return View();
+        }
+    }
+}
